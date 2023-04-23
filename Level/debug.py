f554d7186b136cab051a0135f6ae3cc0da513e6b
abishpius <60949830+abishpius@users.noreply.github.com>
2023-04-23 19:29:52 +0000

    added player movement and collisions

--- Level/debug.py
+++ Level/debug.py
@@ -1,5 +1,7 @@
+import pygame
+
 pygame.init()
-font = pygame.font.font(None, 30)
+font = pygame.font.Font(None, 30)
 
 
 def debug(info, y=10, x=10):
@@ -7,4 +9,4 @@ def debug(info, y=10, x=10):
     debug_surf = font.render(str(info), True, 'White')
     debug_rect = debug_surf.get_rect(topleft=(x, y))
     pygame.draw.rect(display_surface, 'Black', debug_rect)
-    display_surface.blit(debug_surf, degub_rect)
+    display_surface.blit(debug_surf, debug_rect)
